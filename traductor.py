fcb19d5f7f526f6895b2f49a71e3c3d0aed1f8a1
kikin3112 <121058216+kikin3112@users.noreply.github.com>
2023-08-05 00:41:04 -0500

    Add files via upload

---
+++ traductor.py
@@ -0,0 +1,23 @@
+# traductor.py - Traduce cualquier palabra o texto en los idiomas soportados.
+
+import webbrowser
+from urllib.parse import quote
+
+idiomas = {'1': 'spa',
+           '2': 'eng',
+           '3': 'fra',
+           '4': 'por',
+           '5': 'ger'
+}
+
+idioma_entrada = input('Traducir de: (ESP: 1; ING: 2; FRA: 3; POR: 4; ALE: 5) ')
+idioma_salida = input('Traducir a: (ESP: 1; ING: 2; FRA: 3; POR: 4; ALE: 5) ')
+de = idiomas[idioma_entrada]
+a = idiomas[idioma_salida]
+
+entrada = input('Escribe para traducir: ')
+texto = quote(entrada)
+
+webbrowser.open('https://www.reverso.net/traducci%C3%B3n-texto#sl={}&tl={}&text={}'.format(de,
+                                                                                           a,
+                                                                                           texto))
